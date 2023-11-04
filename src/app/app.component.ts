1f85afbaa526efd443537c27c32a8abb865326f9
Ravi Shingadiya <shingadiyaravi2807@gmail.com>
2023-11-04 16:47:45 +0530

    First Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Base 64 to Json Formatter';
+
+  inputText:string = 'ewoJImlkIjogIjAwMDEiLAoJInR5cGUiOiAiZG9udXQiLAoJIm5hbWUiOiAiQ2FrZSIsCgkicHB1IjogMC41NSwKCSJiYXR0ZXJzIjoKCQl7CgkJCSJiYXR0ZXIiOgoJCQkJWwoJCQkJCXsgImlkIjogIjEwMDEiLCAidHlwZSI6ICJSZWd1bGFyIiB9LAoJCQkJCXsgImlkIjogIjEwMDIiLCAidHlwZSI6ICJDaG9jb2xhdGUiIH0sCgkJCQkJeyAiaWQiOiAiMTAwMyIsICJ0eXBlIjogIkJsdWViZXJyeSIgfSwKCQkJCQl7ICJpZCI6ICIxMDA0IiwgInR5cGUiOiAiRGV2aWwncyBGb29kIiB9CgkJCQldCgkJfSwKCSJ0b3BwaW5nIjoKCQlbCgkJCXsgImlkIjogIjUwMDEiLCAidHlwZSI6ICJOb25lIiB9LAoJCQl7ICJpZCI6ICI1MDAyIiwgInR5cGUiOiAiR2xhemVkIiB9LAoJCQl7ICJpZCI6ICI1MDA1IiwgInR5cGUiOiAiU3VnYXIiIH0sCgkJCXsgImlkIjogIjUwMDciLCAidHlwZSI6ICJQb3dkZXJlZCBTdWdhciIgfSwKCQkJeyAiaWQiOiAiNTAwNiIsICJ0eXBlIjogIkNob2NvbGF0ZSB3aXRoIFNwcmlua2xlcyIgfSwKCQkJeyAiaWQiOiAiNTAwMyIsICJ0eXBlIjogIkNob2NvbGF0ZSIgfSwKCQkJeyAiaWQiOiAiNTAwNCIsICJ0eXBlIjogIk1hcGxlIiB9CgkJXQp9';
+  outputText:any = undefined;
+  test:boolean = false;
+
+  ngDoCheck(){
+    this.encodeDecode();
+  }
+
+  encodeDecode(){
+    let regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+    let test = regex.test(this.inputText);
+    // this.outputText = test ? JSON.stringify(JSON.parse(atob(this.inputText)),null,4) : 'Invalid Encoded Input';
+    this.outputText = test ? JSON.parse(atob(this.inputText)) : 'Invalid Encoded Input';
+  }
+}
