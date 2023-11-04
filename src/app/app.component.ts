2de1c1502e3214e2e04dcfe83129810b2513b47e
Ravi Shingadiya <shingadiyaravi2807@gmail.com>
2023-11-04 22:36:37 +0530

    set default string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnChanges,OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,26 +8,33 @@ import { Component,OnChanges,OnInit } from '@angular/core';
 export class AppComponent {
   title = 'Base 64 to Json Formatter';
 
-  inputText:string = '';
-  outputText:object = {};
-  test:boolean = false;
+  constructor() {
+    window.console.log = () => { }
+    window.console.error = () => { }
+  }
+
+  inputText: string = 'W3siaW5mb3JtYXRpb24iOgpbeyJuYW1lIjoiU2hpbmdhZGl5YSBSYXZpIn0seyJkZXBhcnRtZW50Ijoid2ViIiwiZGVzY3JpcHRpb24iOiJBbnkgc3VnZ2VzdGlvbiB3aWxsIGJlIGFjY2VwdGFibGUifV0KfSx7ImRhdGEiOiJudWxsIn1d';
+  outputText: object = {};
+  test: boolean = false;
 
   // ngDoCheck(){
   //   this.encodeDecode();
   // }
 
-  ngOnInit(){
-    this.encodeDecode();
-  }
-  
-  inputValue(event:any){
-    this.inputText = event.target.value;
+  ngOnInit() {
     this.encodeDecode();
   }
 
-  encodeDecode(){
+  inputValue(event: any) {
+    if (event) {
+      this.inputText = event.target.value;
+      this.encodeDecode();
+    }
+  }
+
+  encodeDecode() {
     let regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
     let test = regex.test(this.inputText);
-    this.outputText = test ? JSON.parse(atob(this.inputText)) : 'Unable to display in json format';
+    this.outputText = (test != undefined || test != '') ? JSON.parse(atob(this.inputText)) : 'Unable to display in json format';
   }
 }
